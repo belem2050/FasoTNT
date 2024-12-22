36162fe04e675f4a0a2a68e711e741b4330e9b86
Moumouni BELEM <moumouni.belem@novalynx.fr>
2024-12-22 14:02:23 +0100

    Add project files.

---
+++ FasoTNT/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FasoTNT
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FasoTNT/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FasoTNT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
